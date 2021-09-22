df361989eb83ae0e327fcc5101d154462fe320f7
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-09-22 18:11:42 -0300

    type aliases

---
+++ typeAliases.ts
@@ -0,0 +1,27 @@
+// type alieses
+
+type Uid = number | string | undefined
+
+function logDetails(uid: Uid, item: string) {
+  console.log(`a product ${uid} has a title as ${item}`)
+}
+
+function logInfo(uid: Uid, user: string) {
+  console.log(`a product ${uid} has a title as ${user}`)
+}
+
+logDetails(123, 'sapato')
+logDetails('123', 'sapato')
+
+logInfo(123, 'willian')
+logInfo('123', 'willian')
+
+
+// type aliases
+type Plataform = 'windows' | 'linux' | 'mac'
+
+function renderPlataform(plataform: Plataform) {
+  return plataform
+}
+
+renderPlataform('mac')
